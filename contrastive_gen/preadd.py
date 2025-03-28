f9afc67ae21fdfcd1b017484858f38d18a9fbbf4
JiahuiKChen <jiahui.k.chen@gmail.com>
2025-03-28 10:44:07 -0500

    Greedy preadd generation results for 5 models (Olmo2, Llama, Gemma, Qwen, OPT)

--- contrastive_gen/preadd.py
+++ contrastive_gen/preadd.py
@@ -3,6 +3,7 @@ os.environ['HF_HOME'] = '/datastor1/jiahuikchen/hf_cache'
 
 ### Re-implementation of https://github.com/jonnypei/acl23-preadd/blob/main/methods/method_preadd.py
 ### in a standalone file
+import csv
 import json
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -40,7 +41,9 @@ class ContrastiveGen:
         prefix, 
         preadd_strength=2.0,
         max_tokens=32, 
-        temperature=1, # uses logprobs with no scaling, NOT equivalent to greedy
+        greedy=True, # if true, takes argmax of token logprobs AFTER preadd combination
+        temperature=1, # default uses logprobs with no scaling, NOT equivalent to greedy
+        filter_base_probs=True, # filter base prompt logprobs with top_k and top_p
         top_k=0, 
         top_p=1,
         use_prefix=False
@@ -54,9 +57,11 @@ class ContrastiveGen:
             prefix (str): The control prefix or contrastive sentence to guide generation.
             strength (float): Strength of prefix adaptation.
             max_tokens (int): The maximum number of tokens to generate.
-            temperature (float): Sampling temperature.
-            top_k (int): Top-k sampling value.
-            top_p (float): Top-p sampling value.
+            temperature (float): Sampling temperature, denominator of preadd logprob distribution.
+            filter_base_probs (bool): whether or not to filter prompt's logprobs with top_k, top_p.
+            top_k (int): Top-k sampling value, only applied to prompt's logprobs BEFORE preadd combination.
+            top_p (float): Top-p sampling value, only applied to prompt's logprobs BEFORE preadd combination.
+            use_prefix (bool): if true appends prefix to prompt for contrastive logprobs, otherwise encodes only prefix.
         
         Returns:
             str: The generated text (with the prompt removed).
@@ -77,16 +82,23 @@ class ContrastiveGen:
             prompt_output = self.get_next_logprobs(prompt_ids)
             prefix_output = self.get_next_logprobs(prefix_ids)
 
-            # Apply top-k and top-p filtering to base log probabilities
-            base_logprobs = self.top_k_top_p_filtering(prompt_output["logits"], top_k=top_k, top_p=top_p)
-            # base_logprobs = torch.Tensor(prompt_output['logits']) # no filtering
+            if filter_base_probs:
+                # Apply top-k and top-p filtering to base log probabilities
+                base_logprobs = self.top_k_top_p_filtering(prompt_output["logits"], top_k=top_k, top_p=top_p)
+            else:
+                # Don't apply top k or top p filtering to base generation logprobs
+                base_logprobs = torch.Tensor(prompt_output['logits'])
 
-            # Adjust log probabilities using prefix adaptation
+            # Adjust log probabilities using preadd
             diff = prefix_output["logits"] - base_logprobs
             final_logprobs = (base_logprobs + diff * preadd_strength) / temperature
 
-            # Sample the next token from adjusted probabilities
-            next_token_ind = torch.multinomial(torch.softmax(final_logprobs, dim=-1), num_samples=1).item()
+            # Take the highest probability token if greedy
+            if greedy:
+                next_token_ind = torch.argmax(torch.softmax(final_logprobs, dim=-1)).item()  
+            else:
+                # Sample the next token if not greedy 
+                next_token_ind = torch.multinomial(torch.softmax(final_logprobs, dim=-1), num_samples=1).item()
             next_token = torch.tensor([[next_token_ind]]).to(self.model.device)
 
             # Append the token to both prompt and prefix sequences
@@ -197,15 +209,17 @@ def test_sentiment_control():
         json.dump(preadd_test_gens, open('prefix_tests/preadd_simple_test.json', 'w'), indent=4)
 
 
-def contrastive_gen(
+def run_contrastive_gen(
     base_sentence, 
     contrast_sentence, 
+    greedy,
     preadd_strength=2.0,
     model_string="facebook/opt-6.7b",
     max_tokens=64,
     temperature=1, 
     top_k=0, 
     top_p=1,
+    use_prefix=False
 ):
     """
     Given 2 sentences (additive in relation to each other)
@@ -221,7 +235,7 @@ def contrastive_gen(
     # Baseline gen for base sentence
     generated_text = preadd.generate(
         base_sentence,
-        do_sample=True,
+        do_sample=True if not greedy else False,
         temperature=temperature, 
         top_k=top_k, 
         top_p=top_p,
@@ -232,7 +246,7 @@ def contrastive_gen(
     # Baseline gen for contrasting sentence
     generated_text = preadd.generate(
         contrast_sentence,
-        do_sample=True,
+        do_sample=True if not greedy else False,
         temperature=temperature, 
         top_k=top_k, 
         top_p=top_p,
@@ -245,8 +259,9 @@ def contrastive_gen(
         base_sentence, 
         contrast_sentence, 
         preadd_strength=preadd_strength,
-        max_tokens=max_tokens, 
-        use_prefix=True
+        max_tokens=max_tokens,
+        greedy=greedy,
+        use_prefix=use_prefix
     )
     print(f"Generation: ``{generated_text}``\n\n")
 
@@ -256,27 +271,132 @@ def contrastive_gen(
         contrast_sentence, 
         preadd_strength=-preadd_strength,
         max_tokens=max_tokens, 
-        use_prefix=True
+        greedy=greedy,
+        use_prefix=use_prefix
     )
     print(f"Generation: ``{generated_text}``")
 
 
+def just_contrastive_gen(
+    greedy=True, 
+    preadd_strength=2.0,
+    models = ["allenai/OLMo-2-1124-7B", "meta-llama/Llama-3.1-8B", "facebook/opt-6.7b"]
+):
+    """
+    For all 60 "just" sentences, generates baseline completions for each sentence
+    and 2 preadd completions (encouraging and discouraging "just")
+    """
+    # Sentence prompts
+    with open('/datastor1/jiahuikchen/Just_LLMs/just_sentences.csv', 'r') as f:
+        data = list(csv.DictReader(f))
+        w_just = [row['w_just'] for row in data]
+        wo_just = [row['wo_just'] for row in data]
+
+    # Extract model names 
+    model_parts = [model.split('/')[-1] for model in models]
+
+    for model_part, model_string in zip(model_parts, models):
+        # Create a TSV file for the model 
+        tsv_filename = f"{model_part}_{preadd_strength}greedy.tsv"
+        with open(tsv_filename, 'w', newline='') as file:
+            writer = csv.writer(file, delimiter='\t')
+            writer.writerow(["Just", "JustEncouragedPreadd", "JustNoPreadd", "NoJust", "JustDiscouragedPreadd", "NoJustNoPreadd"])  # Header row
+        
+        preadd = ContrastiveGen(model_string)  
+        max_tokens=64
+        temperature = 1.0 # not used
+        top_k=0
+        top_p=1
+
+        # Baseline and preadd generations for all just minimal pair sentences
+        for sentence_i in range(len(w_just)):
+            just_sentence = w_just[sentence_i]
+            no_just_sentence = wo_just[sentence_i]
+
+            # Preadd generation encouraging just sentence
+            just_preadd_enc = preadd.contrastive_generate(
+                no_just_sentence, 
+                just_sentence, 
+                preadd_strength=preadd_strength,
+                max_tokens=max_tokens,
+                greedy=greedy,
+                use_prefix=False
+            )
+
+            # Baseline gen for just sentence
+            just_baseline = preadd.generate(
+                just_sentence,
+                do_sample=True if not greedy else False,
+                temperature=temperature, 
+                top_k=top_k, 
+                top_p=top_p,
+                max_tokens=max_tokens
+            ) 
+
+            # Preadd generation for discouraging just sentence
+            just_preadd_disc = preadd.contrastive_generate(
+                no_just_sentence, 
+                just_sentence, 
+                preadd_strength=-preadd_strength,
+                max_tokens=64,
+                greedy=greedy,
+                use_prefix=False
+            )            
+
+            # Baseline gen for no just sentence
+            no_just_baseline = preadd.generate(
+                no_just_sentence,
+                do_sample=True if not greedy else False,
+                temperature=temperature, 
+                top_k=top_k, 
+                top_p=top_p,
+                max_tokens=max_tokens
+            ) 
+
+            # Append generated_text to the created TSV file
+            with open(tsv_filename, 'a', newline='') as file:
+                writer = csv.writer(file, delimiter='\t')
+                writer.writerow([just_sentence, just_preadd_enc, just_baseline, no_just_sentence, just_preadd_disc, no_just_baseline])
+
+
 if __name__ == "__main__":
-    model_string = "facebook/opt-6.7b"  # Replace with your desired Hugging Face model
     # "EleutherAI/gpt-j-6B" # other model they experiment with
 
     # Test implementation by using prompt prefixes for sentiment control
     # test_sentiment_control() 
 
     # Contrastive generation with 'test' sentences
-    contrastive_gen(
-        'Betsy\'s picky, she eats chicken nuggets.', 
-        'Betsy\'s picky, she just eats chicken nuggets.', 
-        preadd_strength=2.0
-    )
+    # run_contrastive_gen(
+    #     'I saw a cat', 
+    #     'I saw a dog', 
+    #     preadd_strength=2.0,
+    #     greedy=False,
+    #     use_prefix=False
+    # )
 
-    # contrastive_gen(
+    # # Betsy "just" test
+    # run_contrastive_gen(
+    #     'Betsy\'s picky, she eats chicken nuggets.', 
+    #     'Betsy\'s picky, she just eats chicken nuggets.', 
+    #     preadd_strength=2.0,
+    #     greedy=True,
+    #     use_prefix=False
+    # )
+
+    # # Yvonne "just" test
+    # run_contrastive_gen(
     #     'Yvonne thinks about becoming an astronaut.', 
     #     'Yvonne just thinks about becoming an astronaut.', 
-    #     preadd_strength=2.0
+    #     preadd_strength=2.0,
+    #     greedy=True,
+    #     use_prefix=False
     # )
+
+    # All "just" minimal pair generation runs
+    # just_contrastive_gen(models=["allenai/OLMo-2-1124-7B"])
+    # just_contrastive_gen(models=["meta-llama/Llama-3.1-8B"])
+    # just_contrastive_gen(models=["google/gemma-7b"])
+    # just_contrastive_gen(models=["Qwen/Qwen2.5-7B"]) 
+    just_contrastive_gen(models=["facebook/opt-6.7b"]) 
+
+    # TODO: add conversation + explicit question to prompt
